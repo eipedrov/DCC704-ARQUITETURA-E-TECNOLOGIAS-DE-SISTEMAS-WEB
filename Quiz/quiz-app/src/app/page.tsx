0cbd9a891ea6a907f017a538c21ae046329a75a7
Pedro Vinicius <silvapv7@gmail.com>
2023-09-23 17:12:12 -0400

    user route

--- Quiz/quiz-app/src/app/page.tsx
+++ Quiz/quiz-app/src/app/page.tsx
@@ -4,6 +4,7 @@
 import { FormEventHandler, useState } from "react"
 import { useRouter } from 'next/navigation'
 import { useGlobalContext } from "@/context/main";
+import Link from "next/link";
 
 
 export default function Home() {
@@ -15,8 +16,14 @@ export default function Home() {
 
   const router = useRouter();
 
-  const handleSubmitIniciar: FormEventHandler<HTMLFormElement> = (e) => {
+  const handleSubmitIniciar: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
+
+
+    const { url } = await fetch("http://localhost:3000/api/user", {
+      method: "POST",
+      body: JSON.stringify({ email: newEmail, name: newName }),
+    });
     router.push("/quiz");
   }
 
@@ -30,16 +37,16 @@ export default function Home() {
       <div>
         <form onSubmit={handleSubmitIniciar} className="flex flex-col items-center gap-2">
           <div className="w-96" >
-          <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"></label>
-              <input
-                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                type="text"
-                name="newName"
-                id="newName"
-                value={newName}
-                onChange={(e) => setNewName(e.target.value)}
-                placeholder="Nome: "
-                required />
+            <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"></label>
+            <input
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              type="text"
+              name="newName"
+              id="newName"
+              value={newName}
+              onChange={(e) => setNewName(e.target.value)}
+              placeholder="Nome: "
+              required />
           </div>
           <div className="relative z-0 w-full mb-6 group py-2">
             <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"></label>
@@ -57,9 +64,11 @@ export default function Home() {
             <button type="submit" className="bg-orange-400 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded-full my-5">
               Start Quiz
             </button>
-            <button type="submit" className="bg-orange-400 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded-full my-5">
-              Cadastrar Questão
-            </button>
+            <Link href="/newQuestion">
+              <button type="submit" className="bg-orange-400 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded-full my-5">
+                Cadastrar Questão
+              </button>
+            </Link>
           </div>
         </form>
       </div>
